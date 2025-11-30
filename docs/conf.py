a32d9706cf3b968bd15b0ee3807342226bec1a7d
Richard Russon <rich@flatcap.org>
2025-11-30 01:27:48 +0000

    readthedocs template

---
+++ docs/conf.py
@@ -0,0 +1,45 @@
+# Configuration file for the Sphinx documentation builder.
+
+# -- Project information
+
+project = 'NeoMutt'
+copyright = '2025, Richard Russon'
+author = 'flatcap'
+
+release = '0.1'
+version = '0.1.0'
+
+# -- General configuration
+
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+    'myst_parser',
+]
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+intersphinx_disabled_domains = ['std']
+
+templates_path = ['_templates']
+
+# -- Options for HTML output
+
+html_theme = 'piccolo_theme'
+html_static_path = ["_static"]
+
+# -- Options for EPUB output
+epub_show_urls = 'footnote'
+
+html_context = {
+    "display_github": True, # Integrate GitHub
+    "github_user": "flatcap", # Username
+    "github_repo": "test-read", # Repo name
+    "github_version": "main", # Version
+    "conf_py_path": "/docs/", # Path in the checkout to the docs root
+}
---
+++ docs/index.md
@@ -0,0 +1,19 @@
+﻿---
+title: Lua Docs
+description: NeoMutt Lua Integration
+keywords: [neomutt, lua, scripting]
+sidebar: true
+difficulty: Developer
+time: 5 minutes
+diataxis: Tutorial
+last_updated: 2025-11-30
+---
+
+# Lua Docs
+
+```{toctree}
+---
+maxdepth: 2
+---
+```
+
